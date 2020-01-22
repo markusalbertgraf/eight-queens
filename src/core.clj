f3ea925ee19f9211943c4d72d08c4bbb447856bf
Markus Graf <markusalbertgraf@gmail.com>
2020-01-22 11:53:35 +0100

    clean-up and english for github

--- src/core.clj
+++ src/core.clj
@@ -1,50 +1,66 @@
 (ns core
-  "Calculates positin of queens on a chess board with one queen with the number
-  of queens equal to the size of the board in which no queen threatens another.")
+  "Calculates positin of queens on a chess board with the number
+  of queens equal to the number of rows of the board in which no
+  queen threatens another queen.")
 
-(defn increase [pos board]
+(set! *warn-on-reflection* true)
+
+(defn next-board*
   "Helper to find the next board setup"
+  [pos board]
   (when (< pos (count board))
     (if (= (nth board pos) (- (count board) 1))
       (recur (inc pos) (assoc board pos 0))
       (update board pos inc))))
 
-(defn next-board [board]
-  "Takes a board and returns the next setup"
+(defn next-board
+  "Takes a board and returns the next setup.
+  Only return boards with all queens on different
+  y-positions.  This yealds a speedup of 10x."
+  [board]
   (let [size      (count board)
-        new-board (increase 0 board)
+        new-board (next-board* 0 board)
         positions (count (set new-board))]
     (if (= size positions)
       new-board
       (recur new-board))))
 
-(defn threatens? [x1 y1 x2 y2]
+(defn threatens?
   "Returns true if two queens threaten each other"
-  (or (= y1 y2) (= (Math/abs (- y1 y2)) (Math/abs (- x1 x2))) (= x1 x2)))
+  [x1 y1 x2 y2]
+  (or (= y1 y2)
+      (= (Math/abs ^Integer (- y1 y2))
+         (Math/abs ^Integer (- x1 x2)))
+      (= x1 x2) ;; this sould be redundant but is here
+      )) 
 
-(defn safe? [x1 x2 board]
+(defn solved?
   "Checks all pairs of queens for safety."
-  (let [y1   (nth board x1)
-        y2   (nth board x2)
-        size (count board)]
-    (cond
-      (threatens? x1 y1 x2 y2) false
-      (< x2 (- size 1))        (recur x1 (inc x2) board)
-      (and (= x1 (- size 2))
-           (= x2 (- size 1)))  true
-      :else                    (recur (inc x1) (+ 2 x1) board))))
+  ([board]
+   (solved? 0 1 board))
+  ([x1 x2 board]
+   (let [y1   (nth board x1)
+         y2   (nth board x2)
+         size (count board)]
+     (cond
+       (threatens? x1 y1 x2 y2) false
+       (< x2 (- size 1))        (recur x1 (inc x2) board)
+       (and (= x1 (- size 2))
+            (= x2 (- size 1)))  true
+       :else                    (recur (inc x1) (+ 2 x1) board)))))
 
-(defn passt? [board]
-  "Returns true if no queen theatens another queen."
-  (safe? 0 1 board))
-
-(defn los [size]
-  "Takes the size of the board and returns the first possible solution"
-  (loop [board (vec (repeat size 0))]
-    (cond
-      (nil? board)   (println "Keine Lösung")
-      (passt? board) (println "Lösung: " board)
-      :else          (recur (next-board board)))))
+(defn start
+  "Takes the nr of rows of the board and returns the first solution.
+  With optionol second parameter all?, returns all solutions"
+  ([rows] (start rows false))
+  ([rows all?]
+   (loop [board   (vec (repeat rows 0))
+          counter 1]
+     (cond
+       (nil? board)    (println "no more solutions")
+       (solved? board) (do (printf "Solution %4d: %s\n" counter board)
+                           (when all? (recur (next-board board) (inc counter))))
+       :else           (recur (next-board board) counter)))))
 
 (comment
-  (time (los 7)))
+  (time (start 8 true)))
